43685f3494c75f35d91e616acffd3daa5c423d7c
kimbongcheol <solarrrrr1010@gmail.com>
2021-12-10 11:37:56 +0900

    ADD: 시안 변경에 따른 user table 필드 추가

--- users/models.py
+++ users/models.py
@@ -4,16 +4,19 @@ from core.models    import TimeStampModel
 
 
 class User(TimeStampModel):
-    email                      = models.EmailField(max_length=250, unique=True)
-    password                   = models.CharField(max_length=300, null=True)
-    phone_number               = models.CharField(max_length=30)
-    nickname                   = models.CharField(max_length=30, unique=True)
-    profile_image_url          = models.URLField(max_length=2000, null=True)
-    login_method               = models.CharField(max_length=30, null=True)
-    platform_id                = models.CharField(max_length=30, null=True)
-    description                = models.TextField(null=True)
-    reset_token_number         = models.CharField(max_length=30, null=True)
-    user_histories             = models.ManyToManyField('rooms.Room', through="rooms.UserRoomHistory", related_name="users_histories")
+    name               = models.CharField(max_length=30)
+    birth              = models.CharField(max_length=10)
+    email              = models.EmailField(max_length=250, unique=True)
+    password           = models.CharField(max_length=300, null=True)
+    phone_number       = models.CharField(max_length=30)
+    nation             = models.CharField(max_length=30, null=True)
+    nickname           = models.CharField(max_length=30, unique=True)
+    profile_image_url  = models.URLField(max_length=2000, null=True)
+    login_method       = models.CharField(max_length=30, null=True)
+    platform_id        = models.CharField(max_length=30, null=True)
+    description        = models.TextField(null=True)
+    reset_token_number = models.CharField(max_length=30, null=True)
+    user_histories     = models.ManyToManyField('rooms.Room', through="rooms.UserRoomHistory", related_name="users_histories")
     
     class Meta:
         db_table = 'users'
