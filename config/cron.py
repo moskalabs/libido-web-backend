1beea3cbe3a1a57ecf695b40834034d6cf00bc09
Jaewon Jang <jewon119@gmail.com>
2021-12-22 18:24:49 +0900

    Modified: Fix Crontab for duplicated data

--- config/cron.py
+++ config/cron.py
@@ -23,27 +23,28 @@ def popular_videos_get_youtube_api():
     netflix_obj, created = ContentCategory.objects.get_or_create(name = 'netflix')
     watcha_obj, created  = ContentCategory.objects.get_or_create(name = 'watcha')
 
+    count = 0
     for item in items:
-        obj = Content.objects.create(
-            title                 = item['snippet']['title'],
-            content_link_url      = 'https://www.youtube.com/embed/' + item['id'],
-            thumbnails_url        = item['snippet']['thumbnails']['medium']['url'],
-            running_time          = item['contentDetails']['duration'][2:],
-            view_count            = item['statistics'].get('viewCount'),
-            like_count            = item['statistics'].get('likeCount'),
-            dislike_count         = item['statistics'].get('dislikeCount'),
-            channel_id            = item['snippet']['channelId'],
-            channel_title         = item['snippet']['channelTitle'],
-            published_at          = item['snippet']['publishedAt'],
-            content_categories_id = youtube_obj.id,
-        )
-        if item.get('topicDetails'):
-            tags = item['topicDetails']['topicCategories']
-            for tag in tags:
-                tag_obj, created = Tag.objects.get_or_create(name = tag.split('/')[-1])
-                ContentTag.objects.create(
-                    contents_id = obj.id,
-                    tags_id     = tag_obj.id
-                )
+        if not Content.objects.filter(content_link_url = 'https://www.youtube.com/embed/'+item['id']).exists():
+            obj = Content.objects.create(
+                title                 = item['snippet']['title'],
+                content_link_url      = 'https://www.youtube.com/embed/' + item['id'],
+                thumbnails_url        = item['snippet']['thumbnails']['medium']['url'],
+                running_time          = item['contentDetails']['duration'][2:],
+                view_count            = item['statistics'].get('viewCount'),
+                like_count            = item['statistics'].get('likeCount'),
+                dislike_count         = item['statistics'].get('dislikeCount'),
+                channel_id            = item['snippet']['channelId'],
+                channel_title         = item['snippet']['channelTitle'],
+                published_at          = item['snippet']['publishedAt'],
+                content_categories_id = youtube_obj.id,
+            )
+            count += 1
+            
+            if item.get('topicDetails'):
+                tags = item['topicDetails']['topicCategories']
+                for tag in tags:
+                    tag_obj, created = Tag.objects.get_or_create(name = tag.split('/')[-1])
+                    ContentTag.objects.create(contents_id = obj.id, tags_id = tag_obj.id)
 
-    print(f'{datetime.now()} : {len(items)} data created')
+    print(f'{datetime.now()} : {count}/{len(items)} data created')
