e78858038c61e71412056a2908cc469f0db4412f
jihun <ali@alpsoft.io>
2022-01-25 17:03:32 +0900

    스웨거 auth 기능 추가

--- commons/permissions.py
+++ commons/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework import permissions
+from django.contrib.auth.models import AnonymousUser
+from users.models import User
 
 
 class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated
+        if isinstance(request.user, User):
+            return True
+        else:
+            return False
